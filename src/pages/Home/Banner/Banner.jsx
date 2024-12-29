12afecdd8ca94bebd97e4d13c4e61c2d73101b59
Habibur Rahman <habibur88454@gmail.com>
2024-12-29 16:13:02 +0600

    react swiper added

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,41 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Autoplay, Pagination } from 'swiper/modules';
+
+import banner1 from '../../../assets/home/01.jpg'
+import banner2 from '../../../assets/home/02.jpg'
+import banner3 from '../../../assets/home/03.png'
+
+
+const Banner = () => {
+
+    return (
+        <Swiper
+            modules={[Autoplay, Pagination]}
+            autoplay={{
+                delay: 3000,
+                disableOnInteraction: false
+            }}
+            pagination={{ clickable: true }}
+            loop={true}
+            className="mySwiper my-6"
+        >
+            <SwiperSlide>
+                <img src={banner1} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={banner2} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={banner3} alt="" />
+            </SwiperSlide>
+        </Swiper>
+    );
+};
+
+export default Banner;
