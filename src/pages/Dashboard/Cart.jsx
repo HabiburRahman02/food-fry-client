f60d7d5826d36c653a3742ca6a80a9caa03939e7
Habibur Rahman <habibur88454@gmail.com>
2025-01-03 12:29:39 +0600

    load data in card componets

--- src/pages/Dashboard/Cart.jsx
+++ src/pages/Dashboard/Cart.jsx
@@ -1,8 +1,77 @@
+import SectionTitle from "../../components/SectionTitle";
+import useCart from "../../hooks/useCart";
 
 const Cart = () => {
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((prevValue, currentValue) => {
+        return prevValue + currentValue.price
+    }, 0)
     return (
         <div>
-            my cart
+            <SectionTitle
+                heading={"Wanna add more?"}
+                subHeading='My Cart'
+            ></SectionTitle>
+            <div>
+                <div className="flex justify-between">
+                    <h3 className="text-3xl">Orders: {cart.length}</h3>
+                    <h3 className="text-3xl">Total Price: ${totalPrice}</h3>
+                    <button className="btn">Pay</button>
+                </div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <th>Name</th>
+                                <th>Job</th>
+                                <th>Favorite Color</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {
+                                cart.map((item, i) => <tr key={item._id}>
+                                    <th>
+                                        {i + 1}
+                                    </th>
+                                    <td>
+                                        <div className="flex items-center gap-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle h-12 w-12">
+                                                    <img
+                                                        src={item.image}
+                                                        alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold">Hart Hagerty</div>
+                                                <div className="text-sm opacity-50">United States</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        Zemlak, Daniel and Leannon
+                                        <br />
+                                        <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                    </td>
+                                    <td>Purple</td>
+                                    <th>
+                                        <button className="btn btn-ghost btn-xs">details</button>
+                                    </th>
+                                </tr>)
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
