b046d7985768f2937cf79e765d20df1442a95c5a
Habibur Rahman <habibur88454@gmail.com>
2025-01-09 17:19:55 +0600

    verify user and logout

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,35 @@
 import axios from "axios";
+import useAuth from "./useMenu/useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:5000',
 })
 
 const useAxiosSecure = () => {
+    const { logOut } = useAuth();
+    const navigate = useNavigate();
+    axiosInstance.interceptors.request.use(config => {
+        console.log('from interceptors');
+        const token = localStorage.getItem('access-token');
+        config.headers.authorization = `Bearer ${token}`
+        return config
+    }, error => {
+        return Promise.reject(error);
+    })
+
+    // handle 401 and 403 status
+    axiosInstance.interceptors.response.use(function (response) {
+        return response;
+    }, function (error) {
+        const status = error.status;
+        if (status === 401 || status === 403) {
+            logOut();
+            navigate('/login')
+        }
+        return Promise.reject(error);
+    });
+
     return axiosInstance
 };
 
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -2,12 +2,14 @@
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase/firebase.init";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
 
     const createUser = (email, password) => {
@@ -39,7 +41,18 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-
+            if (currentUser) {
+                const userInfo = { email: currentUser?.email };
+                axiosPublic.post('/jwt', userInfo)
+                    .then(data => {
+                        if (data.data.token) {
+                            localStorage.setItem('access-token', data.data.token)
+                        }
+                    })
+            } else {
+                //remove token from local storage
+                localStorage.removeItem('access-token')
+            }
 
             setLoading(false)
             console.log('currentUser', currentUser);
